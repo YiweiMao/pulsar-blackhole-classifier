31fab973ecd54ac0713e4f38062d0b809e3bc134
YiweiMao <46410481+YiweiMao@users.noreply.github.com>
2019-02-17 03:51:52 +1100

    Update server.py

--- server.py
+++ server.py
@@ -41,7 +41,7 @@ PREDICTION_FILE_SRC = path/'static'/'predictions.txt'
 @app.route("/upload", methods=["POST"])
 async def upload(request):
     data = await request.form()
-    img_bytes = (data["img"].read())
+    img_bytes = data["img"]
     bytes = base64.b64decode(img_bytes)
     return predict_from_bytes(bytes)
 
